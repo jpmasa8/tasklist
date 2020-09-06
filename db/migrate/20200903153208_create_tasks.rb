7d4e6bcada32abe92ecdcd744ad420d553953b97
jpmasa8 <kiyokiyopip56@gmail.com>
2020-09-06 04:14:48 +0000

    third commit

--- db/migrate/20200903153208_create_tasks.rb
+++ db/migrate/20200903153208_create_tasks.rb
@@ -1,8 +1,10 @@
 class CreateTasks < ActiveRecord::Migration[5.1]
   def change
     create_table :tasks do |t|
-      t.string :title
-
+      t.string :title, null: false
+      
+      
+      
       t.timestamps
     end
   end
